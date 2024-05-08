530770c95ad92ff0aea3c6d1638aa47702e3c370
Jandre-S <577584@student.belgiumcampus.ac.za>
2024-05-08 18:43:01 +0200

    base

---
+++ src/prepare_data.py
@@ -0,0 +1,10 @@
+
+import warnings
+import pandas as pd
+import numpy as np
+import skimpy as sm
+
+FilePath_raw_data = 
+FilePath_validation = 
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
